2922462c1c790840e04c155e3956930cb5bca406
Wyrafinowany <zajac_dominik@onet.pl>
2019-07-22 08:20:22 +0200

    Create CoinListRow

---
+++ src/components/CoinList/CoinListRow/CoinListRow.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import styleVars from '../../../shared/styles/variables';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: ${styleVars.baseMargin};
+  margin-right: ${styleVars.baseMargin};
+  @media (max-width: 800px) {
+    margin-left: ${styleVars.smallMargin};
+    margin-right: ${styleVars.smallMargin};
+    font-size: 0.8em;
+  }
+`;
+
+const CoinListRow = ({ children }) => {
+    return <Wrapper>{children}</Wrapper>;
+};
+
+CoinListRow.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default CoinListRow;
